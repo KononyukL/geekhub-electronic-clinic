8e52475fb29b0b71a9b31b913d8b8d3341175f9f
KononyukL <liliashpak97@gmail.com>
2023-03-17 14:53:20 +0200

    update dev

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -4,7 +4,7 @@ import { ContainerHome, Title } from './styled';
 const Home = () => {
   return (
     <ContainerHome>
-      <Title>Home Page test</Title>
+      <Title>Home Page </Title>
     </ContainerHome>
   );
 };
