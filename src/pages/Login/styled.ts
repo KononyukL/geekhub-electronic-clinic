cc5bee574a9706f86beb95c903161dfccb32da0b
KononyukL <liliashpak97@gmail.com>
2023-03-20 15:27:26 +0200

    feature create login page

---
+++ src/pages/Login/styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const StyledFormLogin = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ErrorText = styled.p`
+  font-size: 16px;
+  color: red;
+`;
