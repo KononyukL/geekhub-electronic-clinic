debc6eb9a59b1f793b13d22bc9ab8e115d89940d
Олег Білостоцький <oleg220298d@gmail.com>
2023-03-24 23:33:48 +0200

    feature: add pagination

---
+++ src/components/Paginator/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import {Pagination} from "@mui/material";
+
+const Pagination: FC = () => {
+  return <>
+    <Pagination />
+  </>;
+};
+
+export default Pagination;
