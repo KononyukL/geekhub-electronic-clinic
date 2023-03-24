9e541a7a17667ce76160674f50d6472aa99a4b87
Олег Білостоцький <oleg220298d@gmail.com>
2023-03-24 21:43:11 +0200

    feature: add page Doctors

---
+++ src/components/Doctors/Main/styled.ts
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+
+export const GlobalWrapper = styled.main``;
+
+export const MainWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  margin-left: 20px;
+  padding: 25px;
+  background-color: #f4f4f4;
+  border-radius: 10px;
+  margin-bottom: 25px;
+`;
+
+export const Wrapper = styled.div`
+  max-width: 610px;
+`;
+
+export const AboutDoctor = styled.div`
+  display: flex;
+`;
+
+export const PhotoDoctor = styled.div`
+  padding: 0 25px 25px 0;
+`;
+
+export const Img = styled.img`
+  width: 150px;
+  border-radius: 10px;
+`;
+
+export const Container = styled.div``;
+
+export const NameAndRating = styled.div`
+  margin-bottom: 10px;
+`;
+
+export const H2 = styled.h2`
+  display: inline-block;
+  margin-right: 50px;
+  font-weight: 500;
+  font-size: 20px;
+`;
+
+export const Rating = styled.span`
+  font-size: 20px;
+`;
+
+export const Direction = styled.div`
+  font-size: 16px;
+  margin-bottom: 25px;
+`;
+
+export const LowContainer = styled.div`
+  display: flex;
+  margin-top: 25px;
+`;
+
+export const Info = styled.div`
+  margin-right: 30px;
+`;
+
+export const SpanG = styled.div`
+  color: #989898;
+`;
+
+export const Span = styled.div``;
+
+export const Text = styled.p``;
+
+export const Calendar = styled.div`
+  width: 300px;
+  height: 300px;
+`;
