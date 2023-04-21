1ad46382f006ab2b104fd1d7d317f963e6c3182d
KononyukL <liliashpak97@gmail.com>
2023-04-21 18:20:24 +0300

    add configurated auth and profile requests

--- src/routes/pages/DoctorProfile/ProfileDoctor/index.tsx
+++ src/routes/pages/DoctorProfile/ProfileDoctor/index.tsx
@@ -37,7 +37,7 @@ const ProfileDoctor = () => {
       </Box>
       <BoxInfo>
         <DataCardDoctor />
-        <Security email={profile?.email || '-'} sx={{ paddingBottom: '24px' }} />
+        <Security sx={{ paddingBottom: '24px' }} />
         <AdditionalInformationDoctor profile={profile} />
       </BoxInfo>
     </Container>
